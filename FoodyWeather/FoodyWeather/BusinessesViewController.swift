a942a2cfbe69b9a3ff5729e2f020cc51bb3addb6
Abdullah <alseddiq.af@gmail.com>
2020-12-16 22:24:40 +0300

    Prepare table view
    -Prepare table for list
    -added cell content

--- FoodyWeather/FoodyWeather/BusinessesViewController.swift
+++ FoodyWeather/FoodyWeather/BusinessesViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class BusinessesViewController: UIViewController {
 
+    var businessesList: [Business]!
+    @IBOutlet var businessTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +19,17 @@ class BusinessesViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BusinessesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        businessesList.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
