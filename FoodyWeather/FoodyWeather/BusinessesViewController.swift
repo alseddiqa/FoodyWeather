bc32408f5d8c62cbb52cdfd0379ff455d2aa249b
Abdullah <alseddiq.af@gmail.com>
2020-12-16 23:10:21 +0300

    Fixed Business class bug

--- FoodyWeather/FoodyWeather/BusinessesViewController.swift
+++ FoodyWeather/FoodyWeather/BusinessesViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class BusinessesViewController: UIViewController {
 
     var businessesList: [Business]!
+    var yelpApi: YelpAPI!
+    
     @IBOutlet var businessTableView: UITableView!
     
     override func viewDidLoad() {
@@ -28,8 +30,49 @@ extension BusinessesViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell") as! BusinessTableCell
+        
+        let business = businessesList[indexPath.row]
+        
+        // Set name and phone of cell label
+        cell.businessName.text = business.name
+        cell.phoneNumber.text = business.displayPhone
+        
+        // Get reviews
+        let reviews = business.reviewCount
+        cell.reviews.text = String(reviews)
+        
+        // Get categories
+        let categorie = business.categories[0].title
+        cell.category.text = categorie
+        
+        // Set stars images
+        let reviewDouble = 
+            restaurant["rating"] as! Double
+        cell.starsImage.image = Stars.dict[reviewDouble]!
+        
+        // Set Image of restaurant
+        if let imageUrlString = restaurant["image_url"] as? String {
+            let imageUrl = URL(string: imageUrlString)
+            cell.restaurantImage.af.setImage(withURL: imageUrl!)
+        }
+        return cell
+
     }
     
     
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
--- FoodyWeather/FoodyWeather/ViewController.swift
+++ FoodyWeather/FoodyWeather/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let api = YelpAPI(lat: 51.509865, lon: 0.118092)
+        let api = YelpAPI(lat: 37.7670169511878, lon: -122.42184275)
         api.getBusinessListForLocation()
     }
 
