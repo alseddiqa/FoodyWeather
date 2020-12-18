597ae7e33c6ccbda369e36ec8b951a7df7c2f597
Abdullah <alseddiq.af@gmail.com>
2020-12-18 16:40:34 +0300

    Business Detail view
    -Added a scroll view for the detail

---
+++ FoodyWeather/FoodyWeather/BusinessDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BusinessDetailViewController.swift
+//  FoodyWeather
+//
+//  Created by Abdullah Alseddiq on 12/18/20.
+//
+
+import UIKit
+
+class BusinessDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
