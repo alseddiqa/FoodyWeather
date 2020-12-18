0a9372cfd602dfc4b0e8fc0663a80f02adaff51b
Abdullah <alseddiq.af@gmail.com>
2020-12-18 20:29:20 +0300

    Business Detail View
    -added collection view to the detail to display photos from the business

--- FoodyWeather/FoodyWeather/BusinessDetailViewController.swift
+++ FoodyWeather/FoodyWeather/BusinessDetailViewController.swift
@@ -15,31 +15,4 @@ class BusinessDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func handlePhoneNumClick(_ sender: UIButton) {
-        
-        if let phoneNum = sender.titleLabel?.text {
-            phoneCall(to: phoneNum)
-        }
-        
-    }
-    func phoneCall(to phoneNumber:String) {
-
-        if let callURL:URL = URL(string: "tel:\(phoneNumber)") {
-
-            let application:UIApplication = UIApplication.shared
-
-            if (application.canOpenURL(callURL)) {
-                let alert = UIAlertController(title: "Your Title", message: "Do you want call that number?", preferredStyle: .alert)
-                let callAction = UIAlertAction(title: "Call", style: .default, handler: { (action) in
-                    application.open(callURL, options: [:], completionHandler: nil)
-                })
-                let noAction = UIAlertAction(title: "No", style: .cancel, handler: { (action) in
-                    print("Canceled Call")
-                })
-                alert.addAction(callAction)
-                alert.addAction(noAction)
-                self.present(alert, animated: true, completion: nil)
-            }
-        }
-    }
 }
---
+++ FoodyWeather/FoodyWeather/BusinessPhotosViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BusinessPhotosViewController.swift
+//  FoodyWeather
+//
+//  Created by Abdullah Alseddiq on 12/18/20.
+//
+
+import UIKit
+
+class BusinessPhotosViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var businessPhotos: [URL]!
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return businessPhotos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "BusinessPhotoCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PhotoCollectionViewCell
+        
+        let imageUrl = businessPhotos[indexPath.item]
+        cell.imageView.load(url: imageUrl)
+        cell.shadowDecorate()
+        return cell
+    }
+}
