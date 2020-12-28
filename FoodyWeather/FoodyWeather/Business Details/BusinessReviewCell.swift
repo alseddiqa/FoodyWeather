689fcc92afc1c161e7add48c5404ff0f0deeb2b4
Abdullah <alseddiq.af@gmail.com>
2020-12-28 17:29:22 +0300

    Business Reviews
    -Design review cell
    -Container view to display reviews in collection view

---
+++ FoodyWeather/FoodyWeather/Business Details/BusinessReviewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BusinessReviewCell.swift
+//  FoodyWeather
+//
+//  Created by Abdullah Alseddiq on 12/28/20.
+//
+
+import UIKit
+
+class BusinessReviewCell: UICollectionViewCell {
+    
+    @IBOutlet var reviewRating: UILabel!
+    @IBOutlet var reviewLabel: UILabel!
+    
+}
---
+++ FoodyWeather/FoodyWeather/Business Details/ReviewsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewsViewController.swift
+//  FoodyWeather
+//
+//  Created by Abdullah Alseddiq on 12/28/20.
+//
+
+import UIKit
+
+class ReviewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
